25707a6455484448d1d900d5c91f8b20db93f359
stevigno <ndjouonkep@gmail.com>
2023-08-16 11:54:47 -0700

    2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,8 @@
+
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if len(my_list) == 0:
-        return 0
-    set1 = set(my_list)
-    lst1 = list(set1)
-    numb = 0
-
-    for i in lst1:
-        numb = numb + i
-
-    return numb)
+    c = set(my_list)
+    rlt = 0
+    for i in c:
+        rlt += i
+    return(rlt)
