978543aa2327ad78050216b17af9b52ac794ae86
stevigno <ndjouonkep@gmail.com>
2023-08-16 12:09:23 -0700

    6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    """
+    A function that prints a dictionary by ordered keys
+    by stev
+    """
+    keys = list(a_dictionary.keys())
+    keys.sort()
+    for key in keys:
+        print("{}: {}".format(key, a_dictionary[key]))
+
