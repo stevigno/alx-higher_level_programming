f32ceaee16edfb957e15a6e12e80b49c00593c28
stevigno <ndjouonkep@gmail.com>
2023-08-16 12:02:18 -0700

    2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,14 @@
-
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
 
+def uniq_add(my_list=[]):
     """
     A function that adds all unique
     integers in a list (only once for each integer)
     """
-    c = set(my_list)
-    result = 0
-    for i in c:
-        result += i
-    return(result)
+    new_list = []
+    sum = 0
+    for num in my_list:
+        if num not in new_list:
+            sum += num
+            new_list.append(num)
+    return sum
