121c19115a57e9bb96b2fac0d3af8b95fb9d32a2
stevigno <ndjouonkep@gmail.com>
2023-08-28 10:48:50 -0700

    first

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """
+    Divides 2 integers and print the result.
+
+    :param a: First Number.
+    :param b: Seceend Number.
+    :return: The value of the division, otherwise: None.
+    """
+    try:
+        result = a / b
+
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return (result)
