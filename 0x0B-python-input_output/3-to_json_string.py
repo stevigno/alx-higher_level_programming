b22a86c33c570dadb67cfd39bc122268d53613e0
Steve Blondo <58404283+stevigno@users.noreply.github.com>
2023-11-18 21:35:45 +0000

    fix

--- 0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Writes a string-to-JSON function."""
-import json
-
-
-def to_json_string(obj):
-    """Return the JSON representation of a string object."""
-    return json.dumps(obj)
