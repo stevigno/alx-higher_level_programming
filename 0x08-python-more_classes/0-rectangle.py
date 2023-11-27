558c0c6038421ee0f0c7055586b6b01f4c4490e7
stevigno <ndjouonkep@gmail.com>
2023-11-27 10:01:53 -0800

    first

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # 0-rectangle.py
-"""Define an empty Rectangle class """
+"""Define an empty Rectangle class"""
 
 
 class Rectangle:
+    """A rectangle Represention."""
     pass
